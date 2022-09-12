c02ede21e7d2b466b6eeb7417cb6e6d31b4f57cf
Natalia Nevzorova <alyaryzhaya@gmail.com>
2022-09-12 19:10:39 +0300

    two phases

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,31 +1,44 @@
 public class CreditPaymentService {
 
+    public double calculate(double perYear, int credit, int n) { // метод и входные параметры (ставка в год, сумма кредита, количество платежей)
+
+        double perMonth = perYear / 12 / 100; // ставка в месяц
+
+
+        double annuity = credit * ((perMonth * Math.pow(1 + perMonth, n)) / (Math.pow(1 + perMonth, n) - 1));
+
+        return annuity;
+
+
+        //=============================================================
+
+        
+        //n = 12; // количество платежей
+        //credit = 1000000;  // сумма кредита
+        // double a = 1 + perMonth; // число для возведения в степень (1 + perMonth) ^ n;
+        // System.out.println(Math.pow(1 + perMonth, n)); // переменная для степени числа
+
+
+    }
+
     /*
-    1_000_000_000  -сумма кредита
+    1_000_000  -сумма кредита
     1 год = 12 месяцев  -срок кредита
     9,99%  -процентная ставка
-   + P = 9.99 / 12  - ежемес ставка по процентам
+   + P = 9.99 / 12 /100 - ежемес ставка по процентам
     denominator = (1 + month)^12 -1
 
     мес платеж = сумма кредита * (ежемес.ставка по процентам + (ежем ставка по процентам / (1 + ежем ставка)^на сколько мес кредит - 1)
 
-    ежемес ставка = 9,99/12 =
+         */
 
-
-
-     */
-
-//    double p = 9.99 / 12;  //- ежемес ставка по процентам
-//    double n = 12  // на сколько мес кредит
-//    double denominator = 13;
-//
 //    public static int pow(int denominator, int powDenominator) {
-//
+
 //        return (int) Math.pow(p, denominator);
-//    }
+}
 
 
 
-    }
+
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,39 @@
 public class Main {
     public static void main(String[] args) {
 
-        // perMonth ежемес ставка
-        double perMonth = 9.99 / 12 / 100;  // i
-        System.out.println("Ежемесячная ставка:" + perMonth);
+        CreditPaymentService service = new CreditPaymentService();
+
+        double myCredit = service.calculate(9.99, 1000000, 36);
+
+        System.out.printf("Аннуитетный платеж: " + "%.0f", myCredit); // вывод на экран с фразой и округлением
 
 
-        // degree (1 + i)^12
-        double a = 1 + perMonth;
-        double n = 36;
+//===========================================================================
 
 
-        double degree = (double) Math.pow(a, n);  // переменная для числа в степени
-        System.out.println("Знаменатель (1 + i) ^ 12) = " +  degree);
-
-
-        // формула
-        double annuity = 1000000 * ((perMonth * degree) / ( degree - 1));
-
-       //System.out.println("Аннуитетный платеж:" + annuity); // вывод на экран без округления
-        System.out.printf("Аннуитетный платеж:" + "%.0f",annuity); // вывод на экран с фразой и округлением
+//        // perMonth ежемес ставка
+//        double perMonth = 9.99 / 12 / 100;  // i
+//        System.out.println("Ежемесячная ставка:" + perMonth);
+//
+//
+//        // degree (1 + m)^12
+//        double a = 1 + perMonth;
+//        double n = 36;
+//
+//
+//        double degree = (double) Math.pow(a, n);  // переменная для числа в степени
+//        System.out.println("Знаменатель (1 + m) ^ 12) = " +  degree);
+//
+//
+//        // формула
+//        double annuity = 1000000 * ((perMonth * degree) / ( degree - 1));
+//
+//       //System.out.println("Аннуитетный платеж:" + annuity); // вывод на экран без округления
+//        System.out.printf("Аннуитетный платеж:" + "%.0f",annuity); // вывод на экран с фразой и округлением
 
 
     }
 
 
-
-
-
-
-
-    }
+}
 
