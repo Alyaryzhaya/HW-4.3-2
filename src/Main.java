4cbd5e0df1d68bc5267c5d52ff7032f44d073dc4
Natalia Nevzorova <alyaryzhaya@gmail.com>
2022-09-12 15:06:29 +0300

    resolve

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,25 @@ public class Main {
     public static void main(String[] args) {
 
         // perMonth ежемес ставка
-        double perMonth = 9.99 / 12;  // i
+        double perMonth = 9.99 / 12 / 100;  // i
         System.out.println("Ежемесячная ставка:" + perMonth);
 
 
         // degree (1 + i)^12
         double a = 1 + perMonth;
-        double n = 24;
-//        double a = 3;
-//        double n = 3;
+        double n = 36;
+
 
         double degree = (double) Math.pow(a, n);  // переменная для числа в степени
         System.out.println("Знаменатель (1 + i) ^ 12) = " +  degree);
 
 
         // формула
-        int annuity = (int) (1000000000 * ((perMonth * degree) / ( degree - 1)));
-        System.out.println("Аннуитетный платеж равен:" + annuity);
+        double annuity = 1000000 * ((perMonth * degree) / ( degree - 1));
+
+       //System.out.println("Аннуитетный платеж:" + annuity); // вывод на экран без округления
+        System.out.printf("Аннуитетный платеж:" + "%.0f",annuity); // вывод на экран с фразой и округлением
+
 
     }
 
